5f2e2d47b4554e33bf87b82cae07e274ae7592b5
anirudhkkw <anirudh.kkw@gmail.com>
2015-02-07 16:59:26 +0530

    Working compile of app

--- routes/compileCode.js
+++ routes/compileCode.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.post('/', function(req, res, next) {
-	console.log("code la:");
-	console.log(req.body.codeText);
-	request.post('http://api.hackerearth.com/code/compile/',{
+router.post('/', function(req, res, next) {	
+	try{
+		request.post('http://api.hackerearth.com/code/compile/',{
 		form:{
 			client_secret:'a5e55d08e642a7373ff92e73206f77c67efdde16',
 			async:0,
@@ -16,9 +15,22 @@ router.post('/', function(req, res, next) {
 		}
 	}, 
 	function(err,httpResponse,body){
+	if(err){
+		console.log(JSON.parse(body).errors);
+		res.sendStatus(500);
+	}
+		
+
 	 console.log(JSON.parse(body));
+	 res.sendStatus(200);
 	});
-	res.send(200);
+	
+	}	
+	catch(err){
+		console.log(err);
+		res.sendStatus(500);
+	}
+	
 });
 
 module.exports = router;
